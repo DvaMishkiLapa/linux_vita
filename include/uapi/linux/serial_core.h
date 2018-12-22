64588e0bcba2806faf3e17f9cb466cff1f4ecd33
Sergi Granell <xerpi.g.12@gmail.com>
2018-12-22 10:45:49 +0100

    ARM: vita: initial UART console support

--- include/uapi/linux/serial_core.h
+++ include/uapi/linux/serial_core.h
@@ -276,7 +276,7 @@
 /* Freescale LINFlexD UART */
 #define PORT_LINFLEXUART	122
 
-/* Sunplus UART */
-#define PORT_SUNPLUS	123
+/* PlayStation Vita UART */
+#define PORT_VITAUART	123
 
 #endif /* _UAPILINUX_SERIAL_CORE_H */
